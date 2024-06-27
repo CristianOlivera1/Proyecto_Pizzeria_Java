251e510729f4140ee3ca8aa9fc20faefdef05b4d
CristianOlivera1 <221181@unamba.edu.pe>
2024-06-27 16:06:15 -0500

    first commit

---
+++ src/main/java/com/mycompany/proyecto_pizzeria/Proyecto_Pizzeria.java
@@ -0,0 +1,17 @@
+package com.mycompany.proyecto_pizzeria;
+
+/**
+ *
+ * @author Cristian Olivera Chávez
+           Luis Fernando Juarez Peña
+           Amilcar Virto Loayza    
+           Yhury Cristiam Anampa Quispe
+           Giovanni Ttito Ccaccasto
+ */
+public class Proyecto_Pizzeria {
+
+    public static void main(String[] args) {
+        form frm=new form();
+        frm.setVisible(true);
+    }
+}
