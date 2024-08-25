7d40c805cac2052d6449595aa4cf410724b8058c
Cristian Olivera Chávez <221181@unamba.edu.pe>
2024-08-25 09:31:43 -0500

    clases autonomas excepto login
    detalle del pedido, configuracion de los datos de la empresa, conexion a bd, mesas, eventos del teclado, y el login

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    Connection con;
+
+    // Establece y devuelve una conexión a la base de datos
+    public Connection getConnection(){
+        try {
+            String myBD = "jdbc:mysql://localhost:3306/restaurante"; // URL de la base de datos
+            con = DriverManager.getConnection(myBD, "root", ""); // Crea la conexión
+            return con;
+        } catch (SQLException e) {
+            System.out.println(e.toString()); // Imprime el error en caso de excepción
+        }
+        return null; // Devuelve null si ocurre un error
+    }
+}
